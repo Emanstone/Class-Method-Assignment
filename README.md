e5c7ef83d95a26fb077ce93f6e415ffc2692c282
Emanstone <emanstonepro@gmail.com>
2023-05-02 18:15:41 +0100

    Class-Method Assignment

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a python app that can add,debit & delete and another app that sends bulk email
---
+++ email_assign3.py
@@ -0,0 +1,33 @@
+import smtplib
+
+from email.message import EmailMessage
+
+import ssl  # for faster bulk-mail delivery
+
+# COLLECT USER INFO
+Name = input('Name: ')
+sender = input('Sender email: ')
+recipient = input('Recipient emails : ').split(',')
+mypassword = 'ewhkaqtxojttbbub'
+subject = input('Subject: ')
+body = input('email body content: ')
+
+
+# CREATE THE OBJECT OF THE EMAIL CLASS, EMAILMESSAGE ;
+email_me = EmailMessage()
+
+email_me['Subject'] = subject
+email_me['From'] = sender
+email_me['To'] = ','.join(recipient)
+email_me.set_content(body)
+
+context = ssl.create_default_context()
+
+# OPENNING THE CONNECTION TO GMAIL SERVER AND SEND MESSAGE
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as mailme: 
+    mailme.login(sender, mypassword)
+    mailme.sendmail(sender, recipient, email_me.as_string())
+    # mailme.send_message(email_me)
+
+    print('email sent successfully!')
+
