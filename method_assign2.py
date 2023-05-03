89f27f5be093088ff81cc88e8027ea12d9d974a1
Emanstone <127052283+Emanstone@users.noreply.github.com>
2023-05-03 09:01:43 +0100

    Update method_assign2.py

--- method_assign2.py
+++ method_assign2.py
@@ -1,7 +1,6 @@
 # Create a method that will add, debit a user and then delete a function: If user account balance is 0,
 # User Account name method is deleted,thus Account is deleted. 
 
-
 import sys
 
 class Account:
@@ -15,39 +14,52 @@ class Account:
         return self.name
 
     def add(self):
-        print('Your initial balance is:', self.balance)
-        print('Your deposit is:', self.deposit)
         newbal = self.deposit + self.balance
         return newbal
     
-
     def debit(self):
-        print('Your withdrawal amount is:', self.withdrawal)
         tbalance = (self.balance + self.deposit)
         newbal2 = tbalance - self.withdrawal
         if self.withdrawal > tbalance:
-            print('Insufficient balance')
+            print('Your total balance is:',tbalance)
+            print('Your withdrawal amount is:',self.withdrawal)
+            print('Insufficient funds!')
             sys.exit()
         
         elif self.withdrawal <= tbalance:
             return newbal2
 
-
     def delete(self):
+        tbalance = self.balance + self.deposit
+        return tbalance
+    
+# Create a Child Class of the Parent Class
+class Report(Account):
+    def result(self):
+        print('Account name is:',self.name)
+        print('Your initial balance is:',self.balance)
+        print('Your deposit is:',self.deposit)
+        print('Your total balance is:',Report.add(self))
+        print('Your withdrawal amount is:',self.withdrawal)
+
         tbalance = (self.balance + self.deposit)
         if tbalance - self.withdrawal == 0:
+            print('Your current balance is:',Report.debit(self))
             del Account.add
             del Account.acctname
             print('Your Account is deleted')
         else:
-            print('Your Account is still Active')   
+            print('Your current balance is:',Report.debit(self))
+            print('Your Account is still Active') 
 
 
 # Create Object of the class
-myaccount = Account()
-print('Account name is:', Account.acctname(myaccount))
-print('Total balance is:', Account.add(myaccount))
-print('Current balance is:', Account.debit(myaccount))
-Account.delete(myaccount)
+myaccount = Report()
+Report.acctname(myaccount)
+Report.add(myaccount)
+Report.debit(myaccount)
+Report.delete(myaccount)
+
+Report.result(myaccount)
 
 
